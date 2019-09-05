da76073f3d647b42520851d5417c3d88e997621e
Kaustubh Zagade <38424988+kaustubhz@users.noreply.github.com>
2019-09-05 21:08:33 +0530

    Added menu driven approach

--- Player/Main.cpp
+++ Player/Main.cpp
@@ -1,33 +1,62 @@
-#include"Player.h"
+#include"Mobile.h"
 
 int main()
 {
-	int number;
-	cout<<"\nEnter number of players: ";
+	int number,choice,quantity;
+	char ch;
+	string model;
+	cout<<"Enter number of models: ";
 	cin>>number;
-	Player p[number];
-	for(int i=0;i<number;i++)
+	MobilePhone *m=new MobilePhone[number];
+	//MobilePhone::SearchModel(m,number,"A");
+	do
 	{
-		p[i].Accept();
-	}
-	for(int i=0;i<number;i++)
-	{
-		p[i].Display();
-	}
-	Player::SortByAge(p,number);
-	cout<<"\nPlayer list (By age)"<<endl;
-	for(int i=0;i<number;i++)
-	{
-		p[i].Display();
-	}
+		cout<<"\nWelcome to our Mobile forum"<<endl;
+		cout<<"1. Add a mobile"<<endl;
+		cout<<"2. Display all available mobile models"<<endl;
+		cout<<"3. Quantity of mobile model"<<endl;
+		cout<<"4. Availability of model"<<endl;
+		cout<<"Enter your choice: ";
+		cin>>choice;
+		switch(choice)
+		{
+			case 1: 
+				for(int i=0;i<number;i++)
+				{
+					m[i].Accept();
+				}		
+				break;
+			case 2:
+				MobilePhone::DisplayAllModels(m,number);
+				break;
+			case 3:	cout<<"\nEnter mobile model: ";
+				cin>>model;
+				quantity=MobilePhone::SearchModel(m,number,model);
 
-	
-	Player::SortByName(p,number);
-	cout<<"\nPlayers sorted by name :"<<endl;
-	for(int i=0;i<number;i++)
-	{
-		p[i].Display();
-	}
-	
-return 0;
+				if(quantity==-1)
+					cout<<"\nNo model found";
+				else
+					cout<<"\nModel found with quantity: "<<quantity;
+				break;
+			case 4: cout<<"\nEnter mobile model: ";
+				cin>>model;
+				quantity=MobilePhone::SearchModel(m,number,model);
+
+				if(quantity==-1)
+					cout<<"\nNo model found";
+				else if(quantity==0)
+					cout<<"\nModel is unavailable ";
+				else
+					cout<<"\nModel is available";
+				break;
+			default:cout<<"\nPlease enter valid choice";
+				break;
+		}
+		cout<<"\nDo you want to continue: (y/n) ";
+		cin>>ch;
+		cin.get();//Eater for Enter key
+	}while(ch!='n');
+	cout<<"\nGood bye!!!";
+	delete [] m;
+	return 0;
 }
