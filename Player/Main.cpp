ae7e878264e9de6a1d5d4c320f735186707cb79c
Kaustubh Zagade <kaustubhz.kz153@gmail.com>
2019-09-06 19:56:37 +0530

    Right Main.cpp file

--- Player/Main.cpp
+++ Player/Main.cpp
@@ -1,62 +1,33 @@
-#include"Mobile.h"
+#include"Player.h"
 
 int main()
 {
-	int number,choice,quantity;
-	char ch;
-	string model;
-	cout<<"Enter number of models: ";
+	int number;
+	cout<<"\nEnter number of players: ";
 	cin>>number;
-	MobilePhone *m=new MobilePhone[number];
-	//MobilePhone::SearchModel(m,number,"A");
-	do
+	Player p[number];
+	for(int i=0;i<number;i++)
 	{
-		cout<<"\nWelcome to our Mobile forum"<<endl;
-		cout<<"1. Add a mobile"<<endl;
-		cout<<"2. Display all available mobile models"<<endl;
-		cout<<"3. Quantity of mobile model"<<endl;
-		cout<<"4. Availability of model"<<endl;
-		cout<<"Enter your choice: ";
-		cin>>choice;
-		switch(choice)
-		{
-			case 1: 
-				for(int i=0;i<number;i++)
-				{
-					m[i].Accept();
-				}		
-				break;
-			case 2:
-				MobilePhone::DisplayAllModels(m,number);
-				break;
-			case 3:	cout<<"\nEnter mobile model: ";
-				cin>>model;
-				quantity=MobilePhone::SearchModel(m,number,model);
+		p[i].Accept();
+	}
+	for(int i=0;i<number;i++)
+	{
+		p[i].Display();
+	}
+	Player::SortByAge(p,number);
+	cout<<"\nPlayer list (By age)"<<endl;
+	for(int i=0;i<number;i++)
+	{
+		p[i].Display();
+	}
 
-				if(quantity==-1)
-					cout<<"\nNo model found";
-				else
-					cout<<"\nModel found with quantity: "<<quantity;
-				break;
-			case 4: cout<<"\nEnter mobile model: ";
-				cin>>model;
-				quantity=MobilePhone::SearchModel(m,number,model);
-
-				if(quantity==-1)
-					cout<<"\nNo model found";
-				else if(quantity==0)
-					cout<<"\nModel is unavailable ";
-				else
-					cout<<"\nModel is available";
-				break;
-			default:cout<<"\nPlease enter valid choice";
-				break;
-		}
-		cout<<"\nDo you want to continue: (y/n) ";
-		cin>>ch;
-		cin.get();//Eater for Enter key
-	}while(ch!='n');
-	cout<<"\nGood bye!!!";
-	delete [] m;
-	return 0;
+	
+	Player::SortByName(p,number);
+	cout<<"\nPlayers sorted by name :"<<endl;
+	for(int i=0;i<number;i++)
+	{
+		p[i].Display();
+	}
+	
+return 0;
 }
